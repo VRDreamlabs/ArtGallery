eea88ac6121d8ff2fd8e3891a260af107b07848f
VR Dreamlabs <vrdreamlabs@gmail.com>
2021-06-29 21:10:04 -0700

    Upload everything after deleting _project (to be added later).

---
+++ Assets/level_teleport.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Teleport : MonoBehaviour
+{
+    public bool restart = false;
+    public string levelToLoad = "nameOfLevelToLoad";
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (restart)
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+            else
+            {
+                Application.LoadLevel(levelToLoad);
+            }
+        }
+    }
+}
